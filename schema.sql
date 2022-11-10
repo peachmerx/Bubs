b2093a93f5b01dc8993b1963e8ec73022e2b0d67
peach mercader <peachmercader@gmail.com>
2022-11-10 22:02:31 +1100

    db update

--- schema.sql
+++ schema.sql
@@ -7,23 +7,23 @@ CREATE TABLE toys (
     image_url TEXT
 );
 
-DROP TABLE IF EXISTS clothes;
-CREATE TABLE clothes (
-    id SERIAL PRIMARY KEY,
-    brand VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    price INTEGER NOT NULL,
-    image_url TEXT
-);
+-- DROP TABLE IF EXISTS clothes;
+-- CREATE TABLE clothes (
+--     id SERIAL PRIMARY KEY,
+--     brand VARCHAR(50) NOT NULL,
+--     name VARCHAR(50) NOT NULL,
+--     price INTEGER NOT NULL,
+--     image_url TEXT
+-- );
 
-DROP TABLE IF EXISTS books;
-CREATE TABLE books (
-    id SERIAL PRIMARY KEY,
-    brand VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    price INTEGER NOT NULL,
-    image_url TEXT
-);
+-- DROP TABLE IF EXISTS books;
+-- CREATE TABLE books (
+--     id SERIAL PRIMARY KEY,
+--     brand VARCHAR(50) NOT NULL,
+--     name VARCHAR(50) NOT NULL,
+--     price INTEGER NOT NULL,
+--     image_url TEXT
+-- );
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
--- seed.sql
+++ seed.sql
@@ -3,14 +3,14 @@ INSERT INTO toys (brand, name, image_url, price) VALUES ('Tender Leaf', 'Forest 
 INSERT INTO toys (brand, name, image_url, price) VALUES ('Janod', 'Cocoon Walker With Blocks', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/Janod_COCOONWALKERWITHBLOCKS2_460x.jpg?v=1656288561', 18000);
 INSERT INTO toys (brand, name, image_url, price) VALUES ('Olli Ella', 'Dozy Dinkum - Moppet', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/DozyDinkum-Moppet1_460x.jpg?v=1634688224', 5000);
 
-INSERT INTO clothes (brand, name, image_url, price) VALUES ('Wilson and Frenchy', 'Organic Tie Playsuit - Fruity', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/WF2245-03_1_540x.jpg?v=1662536106', 4300);
-INSERT INTO clothes (brand, name, image_url, price) VALUES ('Wilson and Frenchy', 'Crinkle Button Dress - Little Swan', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/WF2271-02_1_540x.jpg?v=1662535392', 3500);
-INSERT INTO clothes (brand, name, image_url, price) VALUES ('Nature Baby', 'Lottie Suit - Peony Sailor Stripe', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/NB15961_Lottie_Suit_Peony_Sailor_Stripe_Front_540x.jpg?v=1658104199', 5000);
-INSERT INTO clothes (brand, name, image_url, price) VALUES ('Nature Baby', 'Lottie Suit - Large Strawberry Fields Peach', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/NB15961_Lottie_Suit_Strawberry_Fields_Print_Front_540x.jpg?v=1658195300', 5000);
+-- INSERT INTO clothes (brand, name, image_url, price) VALUES ('Wilson and Frenchy', 'Organic Tie Playsuit - Fruity', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/WF2245-03_1_540x.jpg?v=1662536106', 4300);
+-- INSERT INTO clothes (brand, name, image_url, price) VALUES ('Wilson and Frenchy', 'Crinkle Button Dress - Little Swan', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/WF2271-02_1_540x.jpg?v=1662535392', 3500);
+-- INSERT INTO clothes (brand, name, image_url, price) VALUES ('Nature Baby', 'Lottie Suit - Peony Sailor Stripe', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/NB15961_Lottie_Suit_Peony_Sailor_Stripe_Front_540x.jpg?v=1658104199', 5000);
+-- INSERT INTO clothes (brand, name, image_url, price) VALUES ('Nature Baby', 'Lottie Suit - Large Strawberry Fields Peach', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/NB15961_Lottie_Suit_Strawberry_Fields_Print_Front_540x.jpg?v=1658195300', 5000);
 
-INSERT INTO books (brand, name, image_url, price) VALUES ('Janod', 'Mix & Match Magnetibook', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/Janod_MAGNETI_BOOKMIX_MATCHANIMALS_72MAGNETS3_460x.jpg?v=1655960912',  3500);
-INSERT INTO books (brand, name, image_url, price) VALUES ('Ingela P Arrhenius', 'Wild Animals', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/IngelaP.Arrhenius_WildAnimals8_460x.jpg?v=1638499563', 1500);
-INSERT INTO books (brand, name, image_url, price) VALUES ('Sophie Beer', 'Love Makes A Family', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/LoveMakesAFamily_Book_460x.jpg?v=1625640187', 1700);
-INSERT INTO books (brand, name, image_url, price) VALUES ('Eric Carle', 'The Very Hungry Caterpillar', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/Theveryhungrycatipillar_play_460x.jpg?v=1625639609', 1500);
+-- INSERT INTO books (brand, name, image_url, price) VALUES ('Janod', 'Mix & Match Magnetibook', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/Janod_MAGNETI_BOOKMIX_MATCHANIMALS_72MAGNETS3_460x.jpg?v=1655960912',  3500);
+-- INSERT INTO books (brand, name, image_url, price) VALUES ('Ingela P Arrhenius', 'Wild Animals', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/IngelaP.Arrhenius_WildAnimals8_460x.jpg?v=1638499563', 1500);
+-- INSERT INTO books (brand, name, image_url, price) VALUES ('Sophie Beer', 'Love Makes A Family', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/LoveMakesAFamily_Book_460x.jpg?v=1625640187', 1700);
+-- INSERT INTO books (brand, name, image_url, price) VALUES ('Eric Carle', 'The Very Hungry Caterpillar', 'https://cdn.shopify.com/s/files/1/0249/5715/7421/products/Theveryhungrycatipillar_play_460x.jpg?v=1625639609', 1500);
 
 INSERT INTO users (name, email, password_hash) VALUES ('Peach', 'peachmercader@gmail.com', '$2b$12$zPKRrhoIst0lWXzyMhAgFe.mXNCo7mRfGesg7EbDNpKAq7zsFmHY6');
